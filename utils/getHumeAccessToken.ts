8c6319d80a862c5c9ebe7bd92e54e02ae89f9a58
Shashank <48438739+shashankshet@users.noreply.github.com>
2024-09-19 21:00:48 +0530

    updated variable name for secretKey

--- utils/getHumeAccessToken.ts
+++ utils/getHumeAccessToken.ts
@@ -4,7 +4,7 @@ import { fetchAccessToken } from "@humeai/voice";
 export const getHumeAccessToken = async () => {
     const accessToken = await fetchAccessToken({
     apiKey: String(process.env.HUME_API_KEY),
-    clientSecret: String(process.env.HUME_CLIENT_SECRET),
+    secretKey: String(process.env.HUME_CLIENT_SECRET),
   });
 
   if (accessToken === 'undefined') {
