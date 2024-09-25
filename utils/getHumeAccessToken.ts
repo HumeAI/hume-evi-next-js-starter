64633273838c06dfcfccca87e4c03c5a7f6fa189
Franc Camps-Febrer <francamps@gmail.com>
2024-09-25 17:09:45 -0400

    Upgrade hume sdk packages, migrate components to use it accordingly (#6)

--- utils/getHumeAccessToken.ts
+++ utils/getHumeAccessToken.ts
@@ -1,15 +1,16 @@
 import 'server-only';
+
 import { fetchAccessToken } from "hume";
 
 export const getHumeAccessToken = async () => {
-    const accessToken = await fetchAccessToken({
+  const accessToken = await fetchAccessToken({
     apiKey: String(process.env.HUME_API_KEY),
     secretKey: String(process.env.HUME_SECRET_KEY),
   });
 
-  if (accessToken === 'undefined') {
+  if (accessToken === "undefined") {
     return null;
   }
 
   return accessToken ?? null;
-}
+};
